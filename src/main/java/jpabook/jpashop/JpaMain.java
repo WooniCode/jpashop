bb6a4bbad0e65ef2daec46d9742fad51210d99b7
woonijung <jdu2038@gmail.com>
2021-05-10 18:17:21 +0900

    다양한 연관관계 매핑 실전 예제

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -17,14 +17,6 @@ public class JpaMain {
 
         try {
 
-            Order order = new Order();
-            em.persist(order);
-//            order.addOrderItem(new OrderItem()); // 양방향 연관관계 사용시(단방향 사용을 추천)
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-
-            em.persist(orderItem);
-
             tx.commit();
         } catch(Exception e) {
             tx.rollback();
