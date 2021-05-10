f25da8bcba3e74bdd0103fa3c42e1dfe7e841900
woonijung <jdu2038@gmail.com>
2021-05-10 15:08:04 +0900

    연관관계 매핑 예제

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,10 +1,11 @@
 package jpabook.jpashop;
 
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
+import com.sun.tools.corba.se.idl.constExpr.Or;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+
+import javax.persistence.*;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -15,6 +16,15 @@ public class JpaMain {
         tx.begin();
 
         try {
+
+            Order order = new Order();
+            em.persist(order);
+//            order.addOrderItem(new OrderItem()); // 양방향 연관관계 사용시(단방향 사용을 추천)
+            OrderItem orderItem = new OrderItem();
+            orderItem.setOrder(order);
+
+            em.persist(orderItem);
+
             tx.commit();
         } catch(Exception e) {
             tx.rollback();
